91dd0c5c78c18f2713e9715cc7ff78d25416ad2f
Zach Durtschi <The.ZAD.One@GMail.com>
2013-01-09 13:38:07 -0700

    Multiscreen Test, AndroidManafest.xml for Zadify
    Finished a Multiscreen tutorial, added it to Learning folder
    Added text file indicating the tutorials' source
    (http://docs.xamarin.com/Android/Guides/Getting_Started)
    Added an AndroidManifest.xml to Zadify
    
    Future commits will now be focused on Zadify itself.

---
+++ MultiScreenTest/MultiScreenTest/FirstActivity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace MultiScreenTest
+{
+    [Activity(Label = "MultiScreenTest", MainLauncher = true, Icon = "@drawable/icon")]
+    public class FirstActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            //Use UI created in Main.axml
+            SetContentView(Resource.Layout.Main);
+
+            var showSecond = FindViewById<Button>(Resource.Id.showSecond);
+            showSecond.Click += (sender, e) =>
+            {
+                var second = new Intent(this, typeof(SecondActivity));
+                second.PutExtra("FirstData", "Data from FirstActivity (ZAD-Man ROX!!!)");
+                StartActivity(second);
+            };
+        }
+    }
+}
+
