b93666835b91cd75dd159d83f344444bf9700941
Zach Durtschi <The.ZAD.One@GMail.com>
2013-01-09 17:28:40 -0700

    Activities Added
    Add all expected activities except for the specific Competition
    activities
    Added buttons to Main Menu and navigation to Goals Menu

---
+++ Zadify/Zadify/MainMenu.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace Zadify
+{
+    [Activity(Label = "Zadify", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainMenu : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.MainMenu);
+
+            var goalsButton = FindViewById<Button>(Resource.Id.GoalsButton);
+            goalsButton.Click += delegate
+                {
+                    StartActivity(typeof(GoalsMenu));
+                };
+
+            var rewardsButton = FindViewById<Button>(Resource.Id.RewardsButton);
+            rewardsButton.Click += delegate
+            {
+                StartActivity(typeof(RewardsMenu));
+            };
+
+            var socialButton = FindViewById<Button>(Resource.Id.SocialButton);
+            socialButton.Click += delegate
+            {
+                StartActivity(typeof(SocialMenu));
+            };
+
+            var settingsButton = FindViewById<Button>(Resource.Id.SettingsButton);
+            settingsButton.Click += delegate
+            {
+                StartActivity(typeof(SettingsMenu));
+            };
+
+        }
+    }
+}
+
