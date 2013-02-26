c19bcb19bcb1723f7244c93b760758ea5381231a
Zach Durtschi <The.ZAD.One@GMail.com>
2013-02-25 21:28:18 -0700

    Monster enum - food
    Missed one in the last commit.

---
+++ Zadify/Zadify/Enums/MonsterFoods.cs
@@ -0,0 +1,37 @@
+namespace Zadify.Enums
+{
+    enum ZombieFoods
+    {
+        Rations,
+        CornedBeef,
+        Apple
+    }
+
+    enum SkeletonFoods
+    {
+        Lutefisk,
+        DriedFish,
+        FriedChicken
+    }
+
+    enum MummyFoods
+    {
+        BurntGiraffe,
+        HoneyJar,
+        GrapeJelly
+    }
+
+    enum RobotFoods
+    {
+        CakeInACan,
+        Spam,
+        Twinkie
+    }
+
+    enum DemonFoods
+    {
+        AngelFoodCake,
+        DeviledEggs,
+        Divinity
+    }
+}
